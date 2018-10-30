e7ae1fba024c9ec2f0183bb7c9726eb9cb170a32
Josh Morony <joshua.morony@gmail.com>
2018-10-30 13:27:05 +1030

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
+import { myEnterAnimation } from '../animations/enter';
+import { myLeaveAnimation } from '../animations/leave';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(private modalCtrl: ModalController){
+
+  }
+
+  launchModal(){
+
+    this.modalCtrl.create({
+      component: ModalPage,
+      enterAnimation: myEnterAnimation,
+      leaveAnimation: myLeaveAnimation
+    }).then((modal) => {
+      modal.present();
+    });
+
+  }
 
 }
